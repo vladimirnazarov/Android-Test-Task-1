1f4305b66667e444125e32bf39c522c055d54817
Vladimir Nazarov <tooldiamond2014@gmail.com>
2022-11-15 00:31:32 +0300

    Second screen initialized

--- app/src/main/java/com/vnazarov/testandroid1/StartFragment.kt
+++ app/src/main/java/com/vnazarov/testandroid1/StartFragment.kt
@@ -4,12 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class StartFragment : Fragment() {
+class StartFragment: Fragment() {
 
     private lateinit var rv: RecyclerView
     private var adapter: RvAdapter? = null
@@ -23,9 +23,9 @@ class StartFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_main_list, container, false)
+        val view = inflater.inflate(R.layout.fragment_list_of_locations, container, false)
 
-        rv = view.findViewById(R.id.recycler_view) as RecyclerView
+        rv = view.findViewById(R.id.list_recycler_view) as RecyclerView
         rv.layoutManager = LinearLayoutManager(context)
 
         updateUI()
@@ -33,32 +33,24 @@ class StartFragment : Fragment() {
         return view
     }
 
-    private inner class RvHolder(view: View) : RecyclerView.ViewHolder(view) {
+    private inner class RvHolder(view: View): RecyclerView.ViewHolder(view){
 
+        init {
+            itemView.setOnClickListener {
+                Navigation.findNavController(requireView()).navigate(R.id.action_startFragment_to_secondFragment)
+            }
+        }
     }
 
-    private inner class RvAdapter() : RecyclerView.Adapter<RvHolder>() {
+    private inner class RvAdapter(var listOfLocations: List<String>): RecyclerView.Adapter<RvHolder>(){
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RvHolder {
             if (viewType == 0) {
-                val startView = layoutInflater.inflate(R.layout.screen_name, parent, false)
-                return RvHolder(startView)
-            } else if (viewType == 1) {
-                val secondView = layoutInflater.inflate(R.layout.location_name, parent, false)
-                return RvHolder(secondView)
-            } else if (viewType == 2) {
-                val view1 = layoutInflater.inflate(R.layout.rv_package_background_1, parent, false)
-                return RvHolder(view1)
-            } else if (viewType == 3) {
-                val view2 = layoutInflater.inflate(R.layout.rv_package_background_2, parent, false)
-                return RvHolder(view2)
-            } else if (viewType == 4) {
-                val view3 = layoutInflater.inflate(R.layout.rv_package_background_3, parent, false)
-                return RvHolder(view3)
+                val view = layoutInflater.inflate(R.layout.screen_name, parent, false)
+                return RvHolder(view)
             } else {
-                val elseView =
-                    layoutInflater.inflate(R.layout.rv_package_background_2, parent, false)
-                return RvHolder(elseView)
+                val view = layoutInflater.inflate(R.layout.location_name, parent, false)
+                return RvHolder(view)
             }
         }
 
@@ -67,26 +59,18 @@ class StartFragment : Fragment() {
         }
 
         override fun getItemViewType(position: Int): Int {
-            if (position == 0) {
-                return 0
-            } else if (position == 1) {
-                return 1
-            } else if (position == 2) {
-                return 2
-            } else if (position == 3) {
-                return 3
-            } else if (position == 4) {
-                return 4
-            } else return 5
+            return if (position == 0) 0
+            else 1
         }
 
         override fun getItemCount(): Int {
-            return 5
+            return listOfLocations.size + 1
         }
     }
 
-    private fun updateUI() {
-        adapter = RvAdapter()
+    private fun updateUI(){
+        val list = listOf<String>("name")
+        adapter = RvAdapter(list)
         rv.adapter = adapter
     }
 }
